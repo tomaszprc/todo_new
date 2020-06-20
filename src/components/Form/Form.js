6dd89210689ba8f73ecb044a2a256d9e1d24b644
unknown <t.prochowicz@pat15.net>
2020-06-20 22:39:34 +0200

    Dodanie obslugi edycji zadan + dodanie modala

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,10 +5,11 @@ import Button from '../Button/Button';
 class Form extends React.Component {
 
     state = {
-        task_name: '',
-        task_description: ''
+        task_name: this.props.taskName,
+        task_description: this.props.taskDescription,
+        task_id: this.props.taskId
     }
-
+    
     handleInput = (e) => {
         this.setState({
             [e.target.name]: e.target.value
@@ -18,7 +19,7 @@ class Form extends React.Component {
     render() {
         return (
             <form 
-                className="w-1/2 m-0 m-auto text-center bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                className="w-1/2 m-0 m-auto text-center bg-white shadow-md rounded px-8 pt-6 pb-8 my-4"
                 onSubmit={this.props.submit}
             >
                 <FormInput 
@@ -35,6 +36,8 @@ class Form extends React.Component {
                 />
                 <Button 
                     value="Wyślij"
+                    name="submit_button"
+                    id={this.state.task_id}
                 />
             </form>
         )
